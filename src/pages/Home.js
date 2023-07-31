b868ede7fe11f2a02a8a6edf38973295ee5aee57
Ayo-Osota <osotaayomikun@gmail.com>
2023-07-31 10:02:30 +0100

    motun-fic=x

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import Contact from "../components/Contact";
 import HeroSection from "../components/HeroSection";
 import ProjectsSection from "../components/ProjectsSection";
 import Testimonial from "../components/Testimonial";
+import motun from '../assets/motun.jpeg';
+import motunSign from '../assets/motun-sign.jpeg';
 
 const Home = () => {
     return (
@@ -10,8 +12,8 @@ const Home = () => {
             <ProjectsSection/>
             <Testimonial/>
             <Contact/>
-            <img src="src/assets/motun.jpeg" alt="" />
-            <img src="src/assets/motun-sign.jpeg" alt="" />
+            <img src={motun} alt="sigh" />
+            <img src={motunSign} alt=""sigh />
         </main>
     )
 }
