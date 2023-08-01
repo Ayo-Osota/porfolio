bd8640525502a1e202c433f7ae6c10889887d9c4
Ayo-Osota <osotaayomikun@gmail.com>
2023-08-01 11:31:21 +0100

    motun-rm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,6 @@ import Contact from "../components/Contact";
 import HeroSection from "../components/HeroSection";
 import ProjectsSection from "../components/ProjectsSection";
 import Testimonial from "../components/Testimonial";
-import motun from '../assets/motun.jpeg';
-import motunSign from '../assets/motun-sign.jpeg';
 
 const Home = () => {
     return (
@@ -12,8 +10,6 @@ const Home = () => {
             <ProjectsSection/>
             <Testimonial/>
             <Contact/>
-            <img src={motun} alt="sigh" />
-            <img src={motunSign} alt=""sigh />
         </main>
     )
 }
